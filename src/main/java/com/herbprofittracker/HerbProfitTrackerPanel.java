c54c96ebdb764d2e434f0f2082a488d4131aabbf
KeesOG <remcohofmanwerk@hotmail.com>
2024-09-28 20:23:47 +0200

    Edited HerbTrackerPanel

--- src/main/java/com/herbprofittracker/HerbProfitTrackerPanel.java
+++ src/main/java/com/herbprofittracker/HerbProfitTrackerPanel.java
@@ -1,7 +1,9 @@
 package com.herbprofittracker;
 
+import net.runelite.api.Client;
 import net.runelite.api.Item;
 import net.runelite.api.ItemID;
+import net.runelite.client.callback.ClientThread;
 import net.runelite.client.game.ItemManager;
 import net.runelite.client.ui.PluginPanel;
 import net.runelite.client.util.AsyncBufferedImage;
@@ -23,12 +25,16 @@ public class HerbProfitTrackerPanel extends PluginPanel {
     private static final Logger log = LoggerFactory.getLogger(HerbProfitTrackerPanel.class);
     @Inject
     private ItemManager itemManager;
+    @Inject
+    private ClientThread clientThread;
 
     //Initialize ArrayList with id's of all grimy herbs.
     private ArrayList<Integer> herbIDs = new ArrayList<>(
             Arrays.asList(199, 201, 203, 205, 207, 209, 211, 213, 215, 217, 219, 2485, 3049, 3051)
     );
 
+
+    private ArrayList<String> herbNames = new ArrayList<>();
     private ArrayList<AsyncBufferedImage> herbImages = new ArrayList<>();
 
     public void init(){
@@ -39,8 +45,14 @@ public class HerbProfitTrackerPanel extends PluginPanel {
 
         //Get item images and put them in ArrayList.
         addItemImagesToList();
-        addItemImagesToPanel(container);
-        addNamesToPanel(container);
+        addNamesToList();
+
+
+        clientThread.invokeLater(()->{
+            addItemImagesToPanel(container);
+
+            addNamesToPanel(container);
+        });
 
         //Add container of JFrame to PluginPanel
         add(container);
@@ -56,15 +68,28 @@ public class HerbProfitTrackerPanel extends PluginPanel {
         log.info(herbImages.size() + "");
     }
 
+    private void addNamesToList(){
+        for(Integer itemID : herbIDs){
+            clientThread.invokeLater(()-> herbNames.add(itemManager.getItemComposition(itemID).getName()));
+        }
+
+    }
+
     private void addNamesToPanel(Container container){
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.fill = GridBagConstraints.BOTH;
         gbc.weightx = 1;
         gbc.weighty = 1;
 
-        for(Integer itemID : herbIDs){
-            JLabel newLabel = new JLabel();
 
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        for(String itemName : herbNames){
+                JLabel newLabel = new JLabel();
+                newLabel.setText(itemName);
+                container.add(newLabel, gbc);
+                gbc.gridy++;
+                log.info(itemName);
         }
     }
 
@@ -72,11 +97,11 @@ public class HerbProfitTrackerPanel extends PluginPanel {
     private void addItemImagesToPanel(Container container){
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.fill = GridBagConstraints.BOTH;
-        gbc.weightx = -0.3;
+        gbc.weightx = 1;
         gbc.weighty = 1;
 
 
-        gbc.gridx = 1;
+        gbc.gridx = 0;
         gbc.gridy = 0;
         for(AsyncBufferedImage image : herbImages){
             JLabel newLabel = new JLabel();
@@ -88,13 +113,4 @@ public class HerbProfitTrackerPanel extends PluginPanel {
 
 
 
-//            for(Integer itemID : herbIDs){
-//        SwingUtilities.invokeLater(() -> {
-//            log.info(itemManager.getItemComposition(itemID).getName());
-//            JLabel newLabel = new JLabel();
-//            newLabel.setText(itemManager.getItemComposition(itemID).getName());
-//            container.add(newLabel, gbc);
-//            gbc.gridy++;
-//        });
-//    }
 }
